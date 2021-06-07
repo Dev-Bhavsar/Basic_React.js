5b392637da2289e3d261865b1f74e72e2adfd77f
Dev-Bhavsar <83423523+Dev-Bhavsar@users.noreply.github.com>
2021-06-07 10:20:51 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  var cur_year = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>Copyright @ {cur_year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript & React.js</h1>
+      <p>
+        Hey Guys!!! This is an amazing bootcamp by ShapeAI and it is really very
+        helpful for being a full stack web developer. It includes everything
+        HTML, CSS, Javascript & React.js
+      </p>
+    </div>
+  );
+}
+
+export default Note;
